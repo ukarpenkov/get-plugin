85bbab1d46f97b3b1ace1c2eae7283dfa2bb715d
ukarpenkov <truloveu@gmail.com>
2024-03-30 13:46:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import {
+  setSliderSettings, initSlider, initTooltips,
+} from "./src/slider-plugin/index.20a44db0";
+
+
+
+let slider1 = new (0, setSliderSettings)("1", "interval", "horizontal", 1, 50, 1, 100, 1, true, true);
+slider1.init();
+
+(0, initSlider)(".slider-page");
+(0, initTooltips)();
